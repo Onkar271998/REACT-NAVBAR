7dd6057040868556367ab5ac7639e12b2c16e207
Onkar Deshmukh <onkar271998@gmail.com>
2022-05-22 01:02:13 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar= () => {
+
+    return (
+  
+      <section class="navbar">
+            <h2 class="logo">LOGOBAKERY</h2>
+            <div class="internal-bar">
+            <div>
+                Service
+            </div>
+            <div>
+                Projects
+            </div>
+            <div>
+                About
+            </div>
+            
+                </div>
+                <div class="navbar-button">
+                    <button >Contact</button> 
+                </div>
+             
+            
+        
+        
+  </section>
+    
+    )
+
+    
+
+}
+export default Navbar
